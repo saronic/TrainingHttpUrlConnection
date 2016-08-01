03cb1a634b924ba1735af7c57b0a48e8741e1cf6
lsx <lsx@163.com>
2016-08-01 15:34:57 +0800

    能显示网页的源码

--- app/src/main/java/com/li/traininghttpurlconnection/MainActivity.java
+++ app/src/main/java/com/li/traininghttpurlconnection/MainActivity.java
@@ -3,16 +3,26 @@ package com.li.traininghttpurlconnection;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mUrlEditText;
     private TextView mResultTextView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +37,69 @@ public class MainActivity extends AppCompatActivity {
         ConnectivityManager activityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = activityManager.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            Toast.makeText(MainActivity.this, "download..............", Toast.LENGTH_SHORT).show();
+            new DownloadWebpageTask().execute(mUrlEditText.getText().toString());
         } else {
             mResultTextView.setText("No network connection available.");
         }
     }
+
+    class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return downloadUrl(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mResultTextView.setText(s);
+        }
+
+        private String downloadUrl(String url) throws IOException {
+            URL myUrl = new URL(url);
+            InputStream is = null;
+            HttpURLConnection conn = null;
+            int len = 500;
+            try {
+                conn = (HttpURLConnection) myUrl.openConnection();
+                conn.setReadTimeout(10000);  /* milliseconds */
+                conn.setConnectTimeout(15000 ); /* milliseconds */
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(TAG, "The response is: " + response);
+                is = conn.getInputStream();
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+
+            return null;
+        }
+
+        private String readIt(InputStream is, int len) throws IOException {
+            Reader reader = null;
+            reader = new InputStreamReader(is, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+    }
+
+
 }
